a0311d998e25ffa49dc9fb2753ea3ffb44a6b943
ari6 <ari6@tpu.ru>
2025-12-06 17:35:18 +0700

    lab_1

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/User/Documents/Korochtutproga3sem/Proga3SemakEPTA
+# Build directory: C:/Users/User/Documents/Korochtutproga3sem/Proga3SemakEPTA
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/User/Documents/Korochtutproga3sem/Proga3SemakEPTA/CMakeLists.txt;77;add_test;C:/Users/User/Documents/Korochtutproga3sem/Proga3SemakEPTA/CMakeLists.txt;0;")
